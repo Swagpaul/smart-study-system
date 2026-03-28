eee8db0401b6533d7beffe7d8944a3c5d76a3b36
Swagata Paul <hi.its.swagata@gmail.com>
2026-03-28 20:29:27 +0530

    updated the requirements.txt for render deploy

--- app.py
+++ app.py
@@ -985,7 +985,8 @@ Return strictly a JSON object:
     })
 
 
+with app.app_context():
+    db.create_all()
+    
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
